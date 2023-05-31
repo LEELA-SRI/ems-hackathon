491936b179337efe97bddae387d76d89bf6e5def
LEELA-SRI <leelasric@gmail.com>
2023-05-31 20:32:28 +0530

    ui update

--- app.py
+++ app.py
@@ -1,117 +1,120 @@
-from flask import Flask,render_template,request,flash,session,redirect,url_for
+from flask import Flask, render_template, request, flash, session, redirect, url_for
 from hashlib import sha256
-import pymongo 
+import pymongo
 from werkzeug.utils import secure_filename
 from PIL import Image
 from pytesseract import pytesseract
 import re
 import os
+from datetime import date
 import datefinder
-
-
-
-
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY']="oohlala"
-app.config['UPLOAD_FOLDER']='static/images/uploaded'
-uri=f"mongodb+srv://trailUsername:trialPassword@trailcluster.dhfoi.mongodb.net/test"
+app.config['SECRET_KEY'] = "oohlala"
+app.config['UPLOAD_FOLDER'] = 'static/images/uploaded'
+uri = f"mongodb+srv://trailUsername:trialPassword@trailcluster.dhfoi.mongodb.net/test"
 
 
 client = pymongo.MongoClient(uri)
 
 
-db=client.avalanche
+db = client.avalanche
 
 
-events_db=db.events
-users_db=db.users
-brochures_db=db.brochures
+events_db = db.events
+users_db = db.users
+brochures_db = db.brochures
 
 
-ALLOWED_EXTS=set(['png','jpg','jpeg','webp'])
+ALLOWED_EXTS = set(['png', 'jpg', 'jpeg', 'webp'])
+
+
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTS
+
 
 def convert(string):
 
-      if string[-2:] == "AM" and string[:2] == "12":
-         return "00" + string[2:-2]
+    if string[-2:] == "AM" and string[:2] == "12":
+        return "00" + string[2:-2]
 
-      elif string[-2:] == "AM":
-         return string[:-2]
+    elif string[-2:] == "AM":
+        return string[:-2]
 
-      elif string[-2:] == "PM" and string[:2] == "12":
-         return string[:-2]
-        
-      else:
-          return str((string[:2]) + '12') + string[2:8]
-      
+    elif string[-2:] == "PM" and string[:2] == "12":
+        return string[:-2]
+
+    else:
+        return str((string[:2]) + '12') + string[2:8]
+
+
 def image_to_text(path_im):
     path_to_tesseract = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
     image_path = path_im
     pytesseract.tesseract_cmd = path_to_tesseract
-    img = Image.open(image_path )
+    img = Image.open(image_path)
     text = pytesseract.image_to_string(img)
-    ocr_text=text
-    print(ocr_text)
+    ocr_text = text
+
     venue_pattern = r'\b(?:[A-Z][a-z]*\s)*[A-Z][a-z]*\s(?:[A-Z][a-z]*\s)*\b(?:Auditorium|Classroom|Hotel|Floor|Venue)\b'
     venues = re.findall(venue_pattern, ocr_text)
-    if len(venues)>0:
-        venues=venues[0]
+    if len(venues) > 0:
+        venues = venues[0]
     else:
-        venues=''
+        venues = ''
 
     matches = datefinder.find_dates(ocr_text)
-    ns=[]
+    ns = []
     for match in matches:
-        s=str(match.date())
+        s = str(match.date())
         if s[8:] in ocr_text:
             ns.append(s)
-    if len(ns)>0:
-        dates=ns[-1]
+    if len(ns) > 0:
+        dates = ns[-1]
     else:
-        dates=''
+        dates = ''
 
     time_pattern = r'\b\d{1,2}:\d{2}\s*(?:AM|PM|am|pm)?\b'
     times = re.findall(time_pattern, ocr_text)
-    if len(times)>0:
+    if len(times) > 0:
         # for i in times:
-        times=convert(times[0]).rstrip()
-            # print(times)
+        times = convert(times[0]).rstrip()
+        # print(times)
     else:
-        times=''
-    
+        times = ''
 
-    return venues,dates,times
+    return venues, dates, times
 
 
-@app.route('/home',methods=['GET','POST'])
+@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def events():
-    
+
     if session.items() and session['email']:
-        var=True
+        var = True
     else:
-        var=False
+        var = False
+    today_date = str(date.today())
+    print(today_date)
+    # today_date='2023-02-06'
+    event = events_db.find()
+    return render_template('events.html', event=event, var=var, today_date=today_date)
 
-    event=events_db.find()
-    return render_template('events.html',event=event,var=var)
 
-
-@app.route('/userlogin',methods=["GET","POST"])
+@app.route('/userlogin', methods=["GET", "POST"])
 def userlogin():
-    if request.method=='POST':
+    if request.method == 'POST':
         email = request.form.get("email")
         password = request.form.get("password")
-        user=users_db.find_one(
-            {'email':email}
-            )
+        user = users_db.find_one(
+            {'email': email}
+        )
         if user:
-            if sha256(str(password).encode()).hexdigest()==user['password']:
+            if sha256(str(password).encode()).hexdigest() == user['password']:
                 session['email'] = email
-                event=db.events.find()
+
                 return redirect(url_for('addevent'))
             else:
                 flash("Incorrect Password")
@@ -120,19 +123,19 @@ def userlogin():
     return render_template('userlogin.html')
 
 
-@app.route('/registerUser',methods=['GET','POST'])
+@app.route('/registerUser', methods=['GET', 'POST'])
 def userregister():
-    if request.method=='POST':
+    if request.method == 'POST':
         email = request.form.get("email")
         password = request.form.get("password")
         cpassword = request.form.get("cpassword")
-        user=users_db.find_one(
-                {'email':email}
-                )
-        if user==None:
-            if password==cpassword:
+        user = users_db.find_one(
+            {'email': email}
+        )
+        if user == None:
+            if password == cpassword:
                 hashpass = sha256(str(password).encode()).hexdigest()
-                users_db.insert_one({'email' : email, 'password' : hashpass})
+                users_db.insert_one({'email': email, 'password': hashpass})
                 session['email'] = email
                 return render_template('userlogin.html')
             else:
@@ -140,6 +143,7 @@ def userregister():
         else:
             flash('Mail-id already exists')
     return render_template('userregister.html')
+
 
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
@@ -150,53 +154,63 @@ def logout():
         return redirect('/home')
 
 
-@app.route("/addevent", methods=["POST","GET"])
+@app.route("/addevent", methods=["POST", "GET"])
 def addevent():
-        if request.method=='POST':
+    if session.items() and session['email']:
+
+        if request.method == 'POST':
             if 'uploaded_file' not in request.files:
                 flash("No file is uploaded")
-            brochure=request.files['uploaded_file']
-            if brochure.filename=='':
+            brochure = request.files['uploaded_file']
+            if brochure.filename == '':
                 flash("No img selected")
             if brochure and allowed_file(brochure.filename):
-                brochure.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(brochure.filename)))
+                brochure.save(os.path.join(os.path.abspath(os.path.dirname(
+                    __file__)), app.config['UPLOAD_FOLDER'], secure_filename(brochure.filename)))
                 brochures_db.insert_one(
                     {
-                    'uploaded_brochure': f'static/images/uploaded/{secure_filename(brochure.filename)}'
+                        'uploaded_brochure': f'static/images/uploaded/{secure_filename(brochure.filename)}'
                     }
                 )
-                venue_event=image_to_text(f'static/images/uploaded/{secure_filename(brochure.filename)}')[0]
-                date_event=image_to_text(f'static/images/uploaded/{secure_filename(brochure.filename)}')[1]
-                time_event=image_to_text(f'static/images/uploaded/{secure_filename(brochure.filename)}')[2]
-                show=True
-                return render_template('addevent.html',poster=f'static/images/uploaded/{secure_filename(brochure.filename)}',venue=venue_event,date=date_event,time=time_event,show=show)
-                
+                venue_event = image_to_text(
+                    f'static/images/uploaded/{secure_filename(brochure.filename)}')[0]
+                date_event = image_to_text(
+                    f'static/images/uploaded/{secure_filename(brochure.filename)}')[1]
+                time_event = image_to_text(
+                    f'static/images/uploaded/{secure_filename(brochure.filename)}')[2]
+                show = True
+                return render_template('addevent.html', poster=f'static/images/uploaded/{secure_filename(brochure.filename)}', venue=venue_event, date=date_event, time=time_event, show=show)
 
         return render_template('addevent.html')
+    else:
+        return redirect('/userlogin')
 
-@app.route('/upload',methods=[ "POST"])
-def upload():  
-    event_name=request.form.get('event_name')
-    event_date=request.form.get('event_date')
-    event_time=request.form.get('event_time')
-    event_venue=request.form.get('event_venue')
-    broc=brochures_db.find().sort('_id',pymongo.DESCENDING).limit(1)
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    event_name = request.form.get('event_name')
+    event_date = request.form.get('event_date')
+    event_time = request.form.get('event_time')
+    event_venue = request.form.get('event_venue')
+    broc = brochures_db.find().sort('_id', pymongo.DESCENDING).limit(1)
     for i in broc:
-        file_path=i['uploaded_brochure']
+        file_path = i['uploaded_brochure']
     events_db.insert_one(
-                    {
-                    'poster':file_path,
-                    'name':event_name,
-                    'venue':event_venue,
-                    'date': event_date ,
-                    'time':event_time
-                    }
-                )
+        {
+            'poster': file_path,
+            'name': event_name,
+            'venue': event_venue,
+            'date': event_date,
+            'time': event_time
+        }
+    )
     return redirect('/home')
+
+
 @app.route('/categories')
 def categories():
-    broc=brochures_db.find().sort('_id',pymongo.DESCENDING).limit(1)
-    return render_template('categories.html',broc=broc)
+    broc = brochures_db.find().sort('_id', pymongo.DESCENDING).limit(1)
+    return render_template('categories.html', broc=broc)
 
 
 if __name__ == '__main__':
--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,69 @@
-window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
-      });
+let show_all_btn=document.querySelector('.filter-all-btn')
+let show_past_btn=document.querySelector('.filter-past-btn')
+let show_ongoing_btn=document.querySelector('.filter-ongoing-btn')
+let show_upcoming_btn=document.querySelector('.filter-upcoming-btn')
+// console.log(show_all_btn,show_past_btn,show_ongoing_btn,show_upcoming_btn);
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+let show_all=document.querySelectorAll('.item')
+let show_past=document.querySelectorAll('.filter-past')
+let show_ongoing=document.querySelectorAll('.filter-ongoing')
+let show_upcoming=document.querySelectorAll('.filter-upcoming')
+// console.log(show_all,show_past,show_upcoming,show_ongoing);
 
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
+show_past_btn.addEventListener('click',function(){
+  show_past_btn.classList.add('filter-active')
+  show_ongoing_btn.classList.remove('filter-active')
+  show_upcoming_btn.classList.remove('filter-active')
+  show_all_btn.classList.remove('filter-active')
 
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
-      }, true);
-    }
+  for (i of show_all){
+    i.classList.add('d-none')
+    // console.log(i);
+  }
+  for (i of show_past){
+    i.classList.remove('d-none')
+  }
+})
 
-  });
+show_ongoing_btn.addEventListener('click',function(){
+  show_ongoing_btn.classList.add('filter-active')
+  show_past_btn.classList.remove('filter-active')
+  show_upcoming_btn.classList.remove('filter-active')
+  show_all_btn.classList.remove('filter-active')
+  for (i of show_all){
+    i.classList.add('d-none')
+    // console.log(i);
+  }
+  for (i of show_ongoing){
+    i.classList.remove('d-none')
+  }
+})
+
+show_upcoming_btn.addEventListener('click',function(){
+  show_upcoming_btn.classList.add('filter-active')
+  show_ongoing_btn.classList.remove('filter-active')
+  show_past_btn.classList.remove('filter-active')
+  show_all_btn.classList.remove('filter-active')
+
+  for (i of show_all){
+    i.classList.add('d-none')
+    // console.log(i);
+  }
+  for (i of show_upcoming){
+    i.classList.remove('d-none')
+  }
+})
+
+show_all_btn.addEventListener('click',function(){
+  show_all_btn.classList.add('filter-active')
+  show_upcoming_btn.classList.remove('filter-active')
+  show_ongoing_btn.classList.remove('filter-active')
+  show_past_btn.classList.remove('filter-active')
+  for (i of show_all){
+    i.classList.remove('d-none')
+    // console.log(i);
+  }
+  // for (i of show_upcoming){
+  //   i.classList.remove('d-none')
+  // }
+})
